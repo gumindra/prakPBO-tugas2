9fa3a3b76d849b78ece98f5b606bcf5fac3e9428
gumindra <indragumilar774@gmail.com>
2022-03-30 00:52:02 +0700

    tugas2

---
+++ src/main/java/uin/co/id/student/service/StudentServiceImpl.java
@@ -0,0 +1,44 @@
+package uin.co.id.student.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uin.co.id.student.entity.Student;
+import uin.co.id.student.repository.StudentRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Optional<Student> findById(Long id) {
+        return studentRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Student> findByEmail(String email) {
+        return studentRepository.findByEmail(email);
+    }
+
+    @Override
+    public Student save(Student std) {
+        return studentRepository.save(std);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
